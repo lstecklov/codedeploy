3213fbd0c6823f10c0af13873b9c391de6538da9
lstecklov <lstecklov@gmail.com>
2016-10-17 17:46:57 -0400

    test

--- scripts/MCstart.sh
+++ scripts/MCstart.sh
@@ -5,11 +5,11 @@ cd /opt/codedeploy
 export PORT=80
 
 if [[ $NODE_ENV == "production" || $NODE_ENV == "staging" ]]; then
-  npm install && npm start 
+  npm install && npm start  &
 elif [[ $NODE_ENV == "test" ]]; then 
   npm test  
 else
-  npm install && npm start
+  npm install && npm start &
 fi
 
 
